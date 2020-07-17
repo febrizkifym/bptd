5f81278c469247bf0ecd70e163ac625895c7dc9b
febrizkifym <febrizkifym@gmail.com>
2020-07-17 10:33:55 +0800

    fix tvinformasi

--- root/resources/views/tvinformasi.blade.php
+++ root/resources/views/tvinformasi.blade.php
@@ -181,7 +181,7 @@
                 <div class="col">
                     <div class="container" style="height:100%">
                         <div class="video-wrapper">
-                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/ym9_WfOxioc?controls=0&autoplay=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;" class="video" allowfullscreen></iframe>
+                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/ym9_WfOxioc?autoplay=1&controls=0&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;" class="video" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
